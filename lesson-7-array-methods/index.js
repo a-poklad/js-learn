dddecc2165021969a9371c739a456cd64af6428c
poklad <aleksandr.poklad@skylum.com>
2021-04-24 21:58:01 +0300

    lesson-7-array-methods

--- lesson-7-array-methods/index.js
+++ lesson-7-array-methods/index.js
@@ -14,22 +14,40 @@ const newArr4 = arr4.sort((item, item2) => obj[item] - obj[item2])
 // console.log(newArr4)
 
 const palindrome = number => {
-    const str = number.toString()
-    let arr = []
-    for (let i = 0; i < str.length; i++) {
-        arr.push(str[i])
-    }
-    const reverseStr = arr.reverse().join('')
 
-    if (str === reverseStr) {
-        return true
-    } else {
-        return false
-    }
+    if (!number) return false
+    const str = number.toString()
+    const reverseStr = str.split('').reverse().join('')
+    return str === reverseStr
 
 }
 
-console.log(palindrome(121))
-console.log(palindrome(-121))
-console.log(palindrome(10))
-console.log(palindrome(101))
+// console.log(palindrome(121))
+// console.log(palindrome(-121))
+// console.log(palindrome(10))
+// console.log(palindrome(101))
+
+
+let obj2 = {
+    a: {
+        b: {
+            c: {
+                d: 4
+            }
+        }
+    }
+}
+obj2 = {}
+let {a: {b: {c: {d = 2} = {}} = {}} = {}} = obj2
+
+let d = obj2?.a?.b?.c?.d || 3
+
+// console.log(d)
+
+let arr = [{a: {b: 4}}]
+
+arr = []
+
+let [{a: {b = 3} = {}} = {}] = arr
+
+// console.log(b)
